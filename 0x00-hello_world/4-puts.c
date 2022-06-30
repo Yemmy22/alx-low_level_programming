063a6f4064962e11e12ecdb9d6755419eede6f5d
Yemmy22 <opeyemiadedigba20@gmail.com>
2022-06-30 22:34:11 +0000

    Update: Print a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	char my_str[] = "Programming is like building a multilingual puzzle";
+	char my_str[] = "\"Programming is like building a multilingual puzzle";
 
-	puts (my_str);
+	puts(my_str);
 	return (0);
 }

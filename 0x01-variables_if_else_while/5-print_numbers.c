b92e2bfda80f61334a135a9f4cde4a1525a9588d
Yemmy22 <opeyemiadedigba20@gmail.com>
2022-07-02 01:50:39 +0000

    Function prints all single decimal numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num1 = 0;
+	int num2 = 9;
+
+	while (num1 <= num2)
+	{
+	printf("%d", num1);
+	num1++;
+	}
+
+	printf("%c", '\n');
+
+	return (0);
+}

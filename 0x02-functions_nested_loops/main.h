5985d3781bcfa3f2f559e6c46b1771132efb312b
Yemmy22 <opeyemiadedigba20@gmail.com>
2022-07-05 13:12:07 +0000

    Function prints _putchar corrected

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,7 @@
 #include <unistd.h>
 
+int _putchar(char c)
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -12,8 +14,3 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
-
-
-
-int _putchar(char c);

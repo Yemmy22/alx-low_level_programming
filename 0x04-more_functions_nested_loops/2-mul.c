6b382155b26e40cdf1b324277cbe59df5129f8f9
Yemmy22 <opeyemiadedigba20@gmail.com>
2022-07-07 18:31:14 +0000

    Function multiplies pair of integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Performs multiplication operation
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

07a4bd39986fa29666c3fea5914313123d0435c0
Yemmy22 <opeyemiadedigba20@gmail.com>
2022-07-13 23:51:33 +0000

    Function prints string from pointer to character input

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints string to stdout
+ * @str: character string pointer
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+		_putchar (*str);
+}

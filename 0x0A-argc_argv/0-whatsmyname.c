577962d1c1755ad8b29b2dbde7a89b8695219437
Yemmy22 <opeyemiadedigba20@gmail.com>
2022-07-22 19:27:22 +0000

    Program prints file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - Entry
+ * @argc: takes count of string arguments
+ * @argv: array of strings to be counted
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	
+	while( i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}

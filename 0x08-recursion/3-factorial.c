1726e6df56a9abb6583ce355af1662ae0f729b4a
Yemmy22 <opeyemiadedigba20@gmail.com>
2022-07-22 02:19:09 +0000

    Recursive function prints the factorial of an integer

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * factorial - prints the factorial of integer input
+ * @n: integer input
+ *
+ * Return: factorial of input
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}

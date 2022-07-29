c72f9ae3422f14a13ee86b12f1a19dcc83ce017c
Yemmy22 <opeyemiadedigba20@gmail.com>
2022-07-29 01:27:54 +0000

    Function creates array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: number of array elements
+ * @size: byte size of each element
+ * Return: pointer to array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *c;
+	void *m;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	m = malloc(nmemb * size);
+	if (m == NULL)
+		return (NULL);
+
+	c = m;
+	i = 0;
+	while (i < nmemb)
+	{
+		c[i] = '\0';
+		i++;
+
+	}
+	return (m);
+}

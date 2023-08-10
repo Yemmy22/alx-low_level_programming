900d2cd4cf573a717837a828fc55e6ed7313e564
Yemmy22 <opeyemiadedigba20@gmail.com>
2023-08-10 22:41:38 +0000

    Prints a string with a newline without prints and puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <string.h>
+/**
+ * main- Entry point
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	int len;
+	char Str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	len = strlen(Str);
+	write(1, Str, len);
+	return (1);
+}

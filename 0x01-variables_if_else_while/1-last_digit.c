8f5478fdb08a623c388437a926fbdb51eb99c28f
Yemmy22 <opeyemiadedigba20@gmail.com>
2023-08-11 16:55:41 +0000

    Prints whether a random number is greater than 5, equal to zero or less than 6 but greater than zero

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main- Entry Point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n, lst_dgt;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lst_dgt = n % 10;
+	if (lst_dgt == 0)
+		printf("Last digit of %d is %d and is 0", n, lst_dgt);
+	else if (lst_dgt > 5)
+		printf("Last digit of %d is %d and is greater than 5", n, lst_dgt);
+	else if (lst_dgt < 6 && lst_dgt != 0)
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, lst_dgt);
+	return (0);
+}

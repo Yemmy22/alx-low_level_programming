c4a9dbafc4e925fd709772e77fbdff291be4fd1d
Yemmy22 <opeyemiadedigba20@gmail.com>
2023-08-12 02:47:21 +0000

    Prints all combinations of 0 - 9 but prints the lesser combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int lwr = 48;
+	int upr = 57;
+	int j;
+
+	while (lwr <= upr)
+	{
+		j = lwr + 1;
+		while (j > lwr && j <= upr)
+		{
+			putchar(lwr);
+			putchar(j++);
+			if (lwr != upr - 1)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		lwr++;
+	}
+	return (0);
+}

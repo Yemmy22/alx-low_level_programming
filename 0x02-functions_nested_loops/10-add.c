520c9a4821694f888c858821ecadf6dfd707b4fe
Yemmy22 <opeyemiadedigba20@gmail.com>
2023-08-16 01:38:18 +0000

    Returns the addition of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds together two input numbers
+ * @Num1: First input
+ * @Num2: Second input
+ *
+ * Return: The value of the addition
+ */
+int add(int Num1, int Num2)
+{
+	int result = Num1 + Num2;
+
+	return (result);
+}

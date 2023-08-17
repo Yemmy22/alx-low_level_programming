2cbfcf1f04d7b359c42ac4b332dc3620cadf9525
Yemmy22 <opeyemiadedigba20@gmail.com>
2023-08-17 15:13:25 +0000

    Prints all number 0-9 except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - Prints all single digit numbers except 2 and 4
+ *
+ * Return: Nothing
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+			_putchar(i);
+	}
+	_putchar('\n');
+}

2759a0bac413c422f779574defa1b1d85e322bb8
Yemmy22 <opeyemiadedigba20@gmail.com>
2023-08-21 19:50:02 +0000

    Converts input pointer value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98- Changes the value of integer pointer to 98
+ * @n: Input integer pointer to be changed
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

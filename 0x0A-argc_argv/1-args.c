54689fc72306b5594bcda52f0e10186013886dd7
Yemmy22 <opeyemiadedigba20@gmail.com>
2023-09-01 20:23:02 +0000

    Prints number of argument passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: Number of string arguments
+ * @argv: Pointer to an array of string argument
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}

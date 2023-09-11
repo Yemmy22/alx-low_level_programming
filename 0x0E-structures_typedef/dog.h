f3907cf1e7f62983c674841be21a31779b236586
Yemmy22 <opeyemiadedigba20@gmail.com>
2023-09-11 20:53:52 +0000

    Defines a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Contains dog data types
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

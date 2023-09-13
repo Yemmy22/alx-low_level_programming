4f7f518036ede637d0ec4a4e17fb851c245a3405
Yemmy22 <opeyemiadedigba20@gmail.com>
2023-09-13 16:02:30 +0000

    Prints string input

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - Prints name.
+ * @name: String to be printed
+ * @f: Function pointer to called functions
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}

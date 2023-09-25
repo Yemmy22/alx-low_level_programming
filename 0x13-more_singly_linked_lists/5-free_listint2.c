ff6fe4262996d0381ab8d55681c26b94d324ff91
Yemmy22 <opeyemiadedigba20@gmail.com>
2023-09-25 16:39:14 +0000

    Free a list address and set it to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - Frees a list address, then sets the address to NULL
+ * @head: Address of the list to be freed
+ *
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *i;
+
+	while (*head != NULL)
+	{
+		i = *head;
+		*head = (*head)->next;
+		free(i);
+	}
+	*head = NULL;
+}
+

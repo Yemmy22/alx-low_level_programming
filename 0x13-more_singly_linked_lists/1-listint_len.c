5c940c4fad8d428770d19e1c9722ced23b26d796
Yemmy22 <opeyemiadedigba20@gmail.com>
2023-09-25 12:42:16 +0000

    Returns the number of elements in a list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_len - Returns the number of elments in a list
+ * @h: Addrress of the list
+ *
+ * Return: Number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}

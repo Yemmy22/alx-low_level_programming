2ae0c958aae4fb250dbc5eb264800214edebcbe5
Yemmy22 <opeyemiadedigba20@gmail.com>
2023-10-05 02:25:53 +0000

    Create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * create_file - Creates a file
+ * @filename: Address of file to be created
+ * @text_content: Address of string to be written in the file
+ *
+ * Return: 1 on success or -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, wrt, i;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | O_TRUNC);
+	if (fd == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
+	i = 0;
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+	wrt = write(fd, text_content, i);
+	if (wrt == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}

070433a0cfec7a2d456c13961b916922cdda4658
Yemmy22 <opeyemiadedigba20@gmail.com>
2023-12-27 17:39:04 +0000

    Fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,6 +9,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *i;
 
+	if (*head == NULL || head == NULL)
+		return;
+
 	while (*head != NULL)
 	{
 		i = *head;
@@ -16,4 +19,3 @@ void free_listint2(listint_t **head)
 		free(i);
 	}
 }
-

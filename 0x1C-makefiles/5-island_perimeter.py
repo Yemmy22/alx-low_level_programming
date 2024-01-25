64f9715401fdeedfec94d280005dd19d32756665
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-01-25 00:57:20 +0000

    Returns the perimeter of a grid

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+A island_perimeter function module.
+'''
+
+
+def island_perimeter(grid):
+    """
+    Return the perimeter of the island in the grid.
+
+    Args:
+        grid: A list of lists, with integers element representing the island.
+
+    Returns: The perimeter of the island.
+    """
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+
+    return perimeter

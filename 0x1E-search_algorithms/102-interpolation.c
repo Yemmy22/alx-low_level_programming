cededf12c2a6562f9efdab0eb0b2239ead67a322
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-07-17 20:30:38 +0000

    Searches for a value in a sorted array of integers using the interpolation search algorithm and returns the index of the value

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,59 @@
+#include "search_algos.h"
+
+/**
+ * min - Determine the minimum of two input integers
+ * @a: First integer
+ * @b: Second integer
+ *
+ * Return: The minimum.
+ */
+
+/*size_t min(size_t a, size_t b)
+{
+	return (a <= b ? a : b);
+}*/
+
+/**
+ * interpolation_search - Searches for a value in an ordered array
+ * with interpolation search algorithm
+ *
+ * @array: Pointer to the first element of the array to be searched
+ * @size: Size of the array
+ * @value: Value to be searched
+ *
+ * Return: The first index where the value is found
+ * or -1 if not found or if array is NULL
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t prev = 0;
+	size_t current = size - 1;
+	size_t index;
+	
+	if (array != NULL)
+	{
+		while (prev <= current && value >= array[prev] && value <= array[current])
+		{
+			if (prev == current)
+			{
+				if (array[prev] == value)
+					return(prev);
+				return (-1);
+			}
+			index = prev + (((double)(current - prev) / (array[current] - array[prev])) * (value - array[prev]));
+			if (index >= size)
+			{
+				printf("Value checked array[%lu] is out of range\n", index);
+				return (-1);
+			}
+			printf("Value checked array[%lu] = [%d]\n", index, array[index]);
+			if (array[index] == value)
+				return (index);
+			if (array[index] < value)
+				prev = index + 1;
+			else
+			       current	= index - 1;
+		}
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,9 +3,11 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 size_t min(size_t a, size_t b);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif

a6c2d7856aa62d630941af9fab4eb8cd6f6f7d22
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-07-18 00:05:28 +0000

    Hot fix

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -29,7 +29,6 @@ int interpolation_search(int *array, size_t size, int value)
 					return (prev);
 				return (-1);
 			}
-
 			index = prev + (((double)(current - prev
 							) / (array[current] - array[prev])) * (value - array[prev]));
 
@@ -38,7 +37,6 @@ int interpolation_search(int *array, size_t size, int value)
 				printf("Value checked array[%lu] is out of range\n", index);
 				return (-1);
 			}
-
 			printf("Value checked array[%lu] = [%d]\n", index, array[index]);
 
 			if (array[index] == value)
@@ -48,6 +46,12 @@ int interpolation_search(int *array, size_t size, int value)
 				prev = index + 1;
 			else
 			       current	= index - 1;
+		}
+		index = prev + (((double)(current - prev) / (
+						array[current] - array[prev])) * (value - array[prev]));
+		{
+			if (index >= size)
+				printf("Value checked array[%lu] is out of range\n", index);
 		}
 	}
 	return (-1);
